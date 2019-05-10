75f1b84cbf37f24a992c49cde551eefb73fe12c3
Pacheco-1997 <vpacheco26@yahoo.com.br>
2019-05-10 13:45:05 -0300

    Estilizando

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
+ 
   <div id="app">
-    
-    <Conversor moeda-a="USD" moeda-b="BRL"/>  
+  <div class="titulo"><h1>Conversor de Moedas</h1></div>  
+    <div class="linha">
+    <Conversor moeda-a="USD" moeda-b="BRL"/>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <Conversor moeda-a="BRL" moeda-b="USD"/>  
+  </div>
   </div>
 </template>
 
@@ -18,11 +23,23 @@ export default {
 
 <style>
 #app {
+  margin: auto;
+  max-width: 800px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center; 
+  -moz-osx-font-smoothing: grayscale;   
   color: #2c3e50;
   margin-top: 60px;
+}
+.linha{
+  background-color:darkseagreen;
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
+  border-radius: 90px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); 
+}
+.titulo{
+    text-align: center;
 }
 </style>
--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="conversor">
         <h2>{{moedaA}} Para {{moedaB}}</h2>
-        <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
+        <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA"></br>
         <input type="button" value="Converter" v-on:click="converter">
+        <input type="button" value="Limpar" v-on:click="limpar">
         <h2>{{moedaB_value}}</h2>
     </div>
     
@@ -45,6 +46,12 @@ export default {
                         
                     
                     })
+        },
+        limpar(){
+
+            this.moedaB_value = 0;
+            this.moedaA_value = null;
+
         }
         
     }  
@@ -54,7 +61,11 @@ export default {
 
 <!-- O scoped serve para limitar o estilo apenas ao arquivo em que a tag é usada-->  
 <style scoped>
-
+        .conversor{
+            padding: 20px;
+            max-width: 300px;
+           /*box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);*/
+        }
 
 
 </style>
